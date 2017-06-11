5967d32e299af5cfc159c79130364979a52f8f82
saikrishnakotnala <saikrishnachinnu8@gmail.com>
2017-06-12 03:51:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,37 @@
-console.log('the bot is starting');
+console.log('The follow bot is starting');
+
 var Twit = require('twit');
- var config =require('./config');
 
-T = new Twit(config);
-tweetpost();
+var config = require('./config');
+var T = new Twit(config);
 
+// Setting up a user stream
 var stream = T.stream('user');
 
-//stream.on('follow',followed);
+// Anytime someone follows me
+stream.on('follow', followed);
 
-function followed(eventMsg){
-    console.log("follow event");
-     
-   // var fs=require('fs');
-   // var json = JSON.stringify(eventMsg,null,2);
-   // fs.writeFile("tweet.json",json);
-    
-    var name = eventMsg.source.name;
-      var screenName = eventMsg.source.screen_name;
-   tweetpost('.@' + screenName + ' thank you!!!!');
+function followed(eventMsg) {
+  console.log("Follow event!");
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  tweetIt('.@' + screenName + ' do you like rainbows?');
 }
 
 
+function tweetIt(txt) {
 
-//tweetpost();
-//setInterval(tweetpost,1000*20);
+	var tweet = {
+	  status: txt
+	}
 
+	T.post('statuses/update', tweet, tweeted);
 
-
-
-function tweetit(){
-var params = {
-    q: 'saikrishna',
-    count: 2 
-}
-T.get('search/tweets',params,gotData);
-      
-function gotData(err, data, response) {
-    var tweets = data.statuses;
-    for (var i=0; i <tweets.length;i++){
-          console.log(tweets[i].text);
-    }
-}
-}
-
-function tweetpost(txt){
-    var r =Math.floor(Math.random()*100);
-
-
-var tweet = {
-    status:txt
-}
-T.post('statuses/update', tweet, tweeted);
-function tweeted(err,data,response){
-    if(err){
-        console.log("something went wrong");
-    }
-    else{
-        console.log("working");
-    }
-}
+	function tweeted(err, data, response) {
+	  if (err) {
+	  	console.log("Something went wwrong!");
+	  } else {
+	    console.log("It worked!");
+	  }
+	}
 }
