75d6a0744e669c71a1a94dac93fae6c8f957837b
saikrishnakotnala <saikrishnachinnu8@gmail.com>
2017-06-12 04:19:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,38 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-
+// Setting up a user stream
+var stream = T.stream('user');
 
 tweetpost();
-setInterval(tweetIt, 1000*40);
+setInterval(tweetpost, 1000*20);
+// Anytime someone follows me
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+  console.log("Follow event!");
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  tweetIt('.@' + screenName + ' thank you for following me!');
+}
 
 
+function tweetIt(txt) {
 
+	var tweet = {
+	  status: txt
+	}
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+	  if (err) {
+	  	console.log("Something went wwrong!");
+	  } else {
+	    console.log("It worked!");
+	  }
+	}
+}
 
 
 function tweetpost() {
